2c16fa3d93241adfc02fec04a85fe1f2213b32a1
b3tamike <b3tamike@f5a186bb-123b-0410-a59a-9bfa05ff1537>
2007-11-14 20:31:50 +0000

    Initial check-in of module netbreak

---
+++ trunk/netbreak/netbreak/AI.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+//my initial concept for the AI from my intelligence report was to use a uniform cost search with iterative deepening
+// the cost would be based on a function which takes into account number of nodes, number of singletons, size of large groups etc.
+// This may be unfeasible, so I'm going to talk to the prof. and make sure its ok,and try to get a basic idea of how to implement it.
+namespace Netbreak
+{
+	
+	
+	class AI
+	{		
+		public AI()
+		{
+			
+		}
+	}
+}
