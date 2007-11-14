abad958a40192c97ee9fbbc111a9f20f14fb5f1e
b3tamike <b3tamike@f5a186bb-123b-0410-a59a-9bfa05ff1537>
2007-11-14 19:50:47 +0000

    Initial check-in of module Chainshot2

---
+++ trunk/Chainshot2/Chainshot.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Chainshot2
+{
+    class Chainshot
+    {
+    	public static void newGame(Grid game)
+    	{
+            bool GameLoop = true;
+
+            while (GameLoop)
+            {
+                game.displayGrid();
+                Console.Write("Enter Move ( as 'x y' ): ");
+                string move = Console.ReadLine();
+                string[] coord = move.Split(' ');
+                if(game.checkMove(Int32.Parse(coord[0]), Int32.Parse(coord[1]))) {
+                
+                    game.removeGroup(Int32.Parse(coord[0]), Int32.Parse(coord[1]));
+                    game.Logger.addLog("MOVE: (" + coord[0] + "," + coord[1] + ")");
+                    game.Logger.addLog("      --Points: " + game.calculatePoints());
+                    
+                } else
+                {
+                    Console.WriteLine("Invalid Move! Try again!");
+                }
+                
+                game.compressGrid();
+                
+                if (game.checkWin())
+                {
+                    GameLoop = false;
+                    Console.WriteLine("You Win!");
+                    game.Logger.addLog("GAME WIN: All bubbles eliminated");
+                    game.Logger.addLog("FINAL POINTS: " + game.Points);
+                    game.Logger.close();
+                    Console.ReadLine();
+                } else if (game.checkLocked())
+                {
+                    GameLoop = false;
+                    //display final game board showing locked game
+                    game.displayGrid();
+                    Console.WriteLine("You Lose!");
+                    game.Logger.addLog("GAME LOSS: Gameboard locked!");
+                    game.Logger.addLog("FINAL POINTS: " + game.Points);
+                    game.Logger.close();
+                    Console.ReadLine();
+                }
+            }
+    	}
+
+        static void Main()
+        {
+            Console.WriteLine(@"
+ChainShot    
+---------
+Eliminate all bubbles from the grid by selecting and deleting
+groups of the same bubble type (2 or more).  
+   	- Moves are specified in an; 'x y' syntax.
+   	- Points are awarded based on the number of bubbles in an
+   	  eliminated group.");
+
+            bool menuLoop = true;
+
+            while (menuLoop)
+            {
+                Console.WriteLine(@"
+Game Menu:
+1) Specify a gameboard file to open.
+2) Randomly Generate a gamemboard (4 bubble types)
+Choice:");
+
+                int input = Int32.Parse(Console.ReadLine());
+                switch (input)
+                {
+                    case 1: bool askfile = true;
+                        string file = "";
+                        while (askfile)
+                        {
+                            askfile = false;
+                            Console.Write("Enter filename of gameboard:");
+                            file = Console.ReadLine();
+                            Console.WriteLine(Directory.GetCurrentDirectory()+ Path.DirectorySeparatorChar +file);
+                            if (!File.Exists(Directory.GetCurrentDirectory()+Path.DirectorySeparatorChar +file))
+                            {
+                                Console.WriteLine("File does not exist, try again.");
+                                askfile = true;
+                            }
+                        }
+                        newGame(new Grid(file));
+                        break;
+
+                    case 2: int x;
+                        Console.Write("Enter the dimension of the grid:");
+                        x = Int32.Parse(Console.ReadLine());
+                        newGame(new Grid(x));
+                        break;
+
+                    case 3: menuLoop = false;
+                        break;
+                    default: Console.WriteLine("Sorry I don't recognize that option, try agian.");
+                        break;
+                }
+
+                Console.Write("Play again? (y/n):");
+                string pa = Console.ReadLine();
+                if (pa != "y")
+                {
+                    menuLoop = false;
+                }
+            }
+        }
+    }
+}
---
+++ trunk/Chainshot2/MoveLogger.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Chainshot2
+{
+    class MoveLogger
+    {
+        private static StreamWriter logFile;
+		private static string fileName = "ChainShotLog-" + (DateTime.Now.ToString().Replace("/","-").Replace(":",".")) + ".log";
+        
+        public MoveLogger()
+        {
+            logFile = new StreamWriter(fileName);
+            logFile.WriteLine("Chainshot Game Log File");
+            logFile.WriteLine("------------------------");
+        }
+
+        public void addLog(string logLine)
+        {
+            logFile.WriteLine(logLine);
+        }
+
+        public void close()
+        {
+            logFile.Close();
+        }
+    }
+}
