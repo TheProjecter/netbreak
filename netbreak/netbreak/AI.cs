fc14f2f94b7f708bbcb44fb06e07e884ada10864
b3tamike <b3tamike@f5a186bb-123b-0410-a59a-9bfa05ff1537>
2007-11-15 07:19:25 +0000

    Implemented Tree and TreeNode. Added stubs for initial AI evaluation.

--- netbreak/netbreak/AI.cs
+++ netbreak/netbreak/AI.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 
 //my initial concept for the AI from my intelligence report was to use a uniform cost search with iterative deepening
@@ -9,10 +10,38 @@ namespace Netbreak
 	
 	
 	class AI
-	{		
-		public AI()
-		{
-			
-		}
+	{
+        private int dimensions;
+
+        public AI(int dim)
+        {
+            this.dimensions = dim;
+        }
+
+        public int[] getMove(string[][] grid)
+        {
+            string[][] nextGrid;
+            int[] point = new int[2];
+
+            Tree tree = new Tree(evaluateBoard(grid));
+            SortedList<int, int> queue = new SortedList<int,int>();
+
+            nextGrid = makeMove(
+
+
+        }
+
+        public int evaluateBoard(string[][] grid)
+        {
+            //do some crazy heuristic evaluation
+            return 0;
+        }
+
+        public int countRemaining(string[][] grid)
+        {
+            //count how many remaining nodes we have
+            return 0;
+        }
 	}
 }
+
