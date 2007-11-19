e5975868d03956cf4d32020f9e4a96fe3d78814a
b3tamike <b3tamike@f5a186bb-123b-0410-a59a-9bfa05ff1537>
2007-11-19 23:13:18 +0000

    Made Generic Priority Queue from the HBLT.  Began reworking AI class

--- trunk/netbreak/netbreak/AI.cs
+++ trunk/netbreak/netbreak/AI.cs
@@ -1,45 +1,25 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
 
-
-//my initial concept for the AI from my intelligence report was to use a uniform cost search with iterative deepening
-// the cost would be based on a function which takes into account number of nodes, number of singletons, size of large groups etc.
-// This may be unfeasible, so I'm going to talk to the prof. and make sure its ok,and try to get a basic idea of how to implement it.
 namespace Netbreak
 {
-	
-	
-	class AI
-	{
-        private int dimensions;
+    class AI
+    {
 
-        public AI(int dim)
+        public void makeNextMove(string[,] grid)
         {
-            this.dimensions = dim;
+            //make new node class for nodes expanded by board tree. this should thus hold the move, and score
+            // moveNode(x,y, rank)
+            PriorityQueue<int> q = new PriorityQueue<int>();
+
+
         }
 
-        public int[] getMove(string[][] grid)
+        public int rankBoard()
         {
-            string[][] nextGrid;
-            int[] point = new int[2];
-
-            Tree tree = new Tree(evaluateBoard(grid));
-            SortedList<int, int> queue = new SortedList<int,int>();
-
-            nextGrid = makeMove();
-        }
-
-        public int evaluateBoard(string[][] grid)
-        {
-            //do some crazy heuristic evaluation
+            //here goes my ranking algorithm!!
             return 0;
         }
-
-        public int countRemaining(string[][] grid)
-        {
-            //count how many remaining nodes we have
-            return 0;
-        }
-	}
+    }
 }
-
